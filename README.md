93ba551baa7c753acc8727997290b4911cffdb5b
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2020-11-24 10:04:03 +0100

    first version of raster map

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Simple-Raster-Map
 coastline map with raster positions, for large data sets
+
+# Simple Raster Map
+MoveApps
+
+Github repository: *github.com/movestore/Simple-Raster-Map*
+
+## Description
+Maps raster of all location points with coastlines background. Fastest option to plot large data sets (>100,000 locations). Start with large grid values to test performance. 
+
+## Documentation
+This App transforms all Movement data locations to a set of location points in an area equal distance projection. Those are then merged into a raster of given grid size and plotted. For orientation coastlines are added. For further analyses the input data set is also returned.
+
+### Input data
+moveStack in Movebank format
+
+### Output data
+Shiny user interface (UI)
+moveStack in Movebank format
+
+### Artefacts
+none
+
+### Parameters 
+`grid`: Integer indicating the grid size for rasterizing your data. Large values give less, large grid cells. Please choose values between 1000 and 300000. This value can also be adapted in the UI. Unit: `m`. Example: 50000.
+
+### Null or error handling:
+**Parameter `grid`:** This parameter has a default of 50000. If you provide a value outside of the provided range (1000 - 300000) the parameter will be mapped to the respective range edge (1000 if value too small, 300000 if value too large).
+
+**Data:** This App is not reducing the data in any form, so no empty tracks can occur. The output data set should have the same dimensions as the input.
+
---
+++ ShinyModule.R
@@ -0,0 +1,58 @@
+library('move')
+library('shiny')
+library('raster')
+library('rgeos')
+
+#setwd("/root/app/")
+
+shinyModuleUserInterface <- function(id, label, grid = 50000) {
+  ns <- NS(id)
+  
+  tagList(
+    titlePanel("Raster map of location density"),
+    sliderInput(inputId = ns("grid"), 
+                label = "Choose a raster grid size in m", 
+                value = grid, min = 1000, max = 300000),
+   plotOutput(ns("map"))
+  )
+}
+
+shinyModuleConfiguration <- function(id, input) {
+  ns <- NS(id)
+  configuration <- list()
+
+  print(ns('grid'))
+  configuration["grid"] <- input[[ns('grid')]]
+
+  configuration
+}
+
+shinyModule <- function(input, output, session, data, grid = 50000) {
+  current <- reactiveVal(data)
+  
+  SP <- SpatialPoints(data,proj4string=CRS("+proj=longlat +ellps=WGS84 +no_defs"))
+  SPT <- spTransform(SP,CRSobj="+proj=aeqd +lat_0=53 +lon_0=24 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs")
+  
+  outputRaster <- reactive({
+    raster(ext=extent(SPT), resolution=input$grid, crs = "+proj=aeqd +lat_0=53 +lon_0=24 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs", vals=NULL)
+  })
+  
+  rasterObjT <- reactive({
+      rasterize(SPT,outputRaster(),fun="count",update=TRUE)
+  })  
+
+
+  coastlines <- readOGR("ne-coastlines-10m/ne_10m_coastline.shp")
+  coastlinesC <- crop(coastlines,extent(SP))
+  coast <- spTransform(coastlinesC,CRSobj="+proj=aeqd +lat_0=53 +lon_0=24 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs")
+
+  
+  output$map <- renderPlot({
+    plot(rasterObjT(),colNA=NA,axes=FALSE,asp=1) 
+    plot(coast, add = TRUE)
+  })
+  
+  return(reactive({ current() }))
+}
+
+
